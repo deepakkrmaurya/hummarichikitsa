ae947811e729dec8c87cb0b372663fc799fd0ac5
deepakkrmaurya <deepakmaurya2211@gmail.com>
2025-07-18 14:18:14 +0530

    Update axiosInstance to uncomment production base URL and comment out local URL

--- src/Helper/axiosInstance.js
+++ src/Helper/axiosInstance.js
@@ -12,8 +12,8 @@
 
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/v1";
-// const BASE_URL = "https://doctor-appointment-backend-t00j.onrender.com/api/v1";
+// const BASE_URL = "http://localhost:5000/api/v1";
+const BASE_URL = "https://doctor-appointment-backend-t00j.onrender.com/api/v1";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
